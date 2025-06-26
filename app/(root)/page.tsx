5208e0e3ee578398a56209b0acd955f26e9de4a4
enigmaSpy <soczekv18@gmail.com>
2025-06-26 22:51:48 +0200

    Refactor Home component layout and update routes for Ask Questions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,30 @@
-import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-const Home = async() => {
-  const session = await auth();
-  console.log(session);
-  
+import Link from "next/link";
+
+const Home = async() => {  
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All questions</h1>
+        <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900" asChild>
+          <Link href={ROUTES.ASK_QUESTIONS}>
+            Ask a question 
+          </Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        LocalSearch
+        HomeFilters
+        <div className="mt-10 flex w-full flex-col gap-6">
+          <p>Question card 1</p>
+          <p>Question card 1</p>
+          <p>Question card 1</p>
+          <p>Question card 1</p>
+          <p>Question card 1</p>
+          <p>Question card 1</p>
+        </div>
+      </section>
     </>
   );
 };
